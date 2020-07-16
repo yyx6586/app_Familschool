4516082e06d370527223be8547a0345852e9ac4b
yyx <1234@qq.com>
2020-07-16 09:43:28 +0800

    first commit

--- app/src/main/java/com/aloogn/view/LoginActivity.java
+++ app/src/main/java/com/aloogn/view/LoginActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aloogn.famil_school.R;
 
@@ -14,14 +17,21 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.BufferedSink;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button login_btn_login;
     TextView login_tv_forget;
+    EditText login_et_account;
+    EditText login_et_password;
+    CheckBox login_cb_remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         login_btn_login = (Button) findViewById(R.id.login_btn_login);
         login_tv_forget = (TextView) findViewById(R.id.login_tv_forget);
+        login_et_account = (EditText) findViewById(R.id.login_et_account);
+        login_et_password = (EditText) findViewById(R.id.login_et_password);
+        login_cb_remember = (CheckBox) findViewById(R.id.login_cb_remember);
 
         login_btn_login.setOnClickListener(this);
         login_tv_forget.setOnClickListener(this);
@@ -39,11 +52,30 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.login_btn_login:
+                String account = login_et_account.getText().toString();
+                String password = login_et_password.getText().toString();
+
+                if(account == null || "".equals(account)){
+                    Toast.makeText(LoginActivity.this,"帐号不能为空",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(password == null || "".equals(password)){
+                    Toast.makeText(LoginActivity.this,"密码不能为空",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 String url = "http://10.0.2.2:8080/loginServlet";
                 OkHttpClient okHttpClient = new OkHttpClient();
+                FormBody formBody = new FormBody
+                        .Builder()
+                        .add("account",account)
+                        .add("password",password)
+                        .build();
+
                 final Request request = new Request.Builder()
                         .url(url)
-                        .get()//默认就是GET请求，可以不写
+                        .post(formBody)//默认就是GET请求，可以不写
                         .build();
                 Call call = okHttpClient.newCall(request);
                 call.enqueue(new Callback() {
